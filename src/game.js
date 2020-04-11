bea6c80356b533a0099e4addfe58f132c81b925e
Maximilian Zirps <maximilian.zirps@gmail.com>
2020-04-11 14:16:55 +0200

    change boy asset to king

--- src/game.js
+++ src/game.js
@@ -16,12 +16,13 @@ function removeIceCream(platform, iceCream) {
 }
 
 function preload() {
+    // this.textures.remove('king')
     this.load.image('background', 'assets/background.png');
     this.load.image('dandelion', 'assets/dandelion.png');
     this.load.image('weed', 'assets/weed.png');
     this.load.image('ground', 'assets/ground.png');
     this.load.image('ice-cream', 'assets/ice-cream.png')
-    this.load.spritesheet('boy', 'assets/boy.png', { frameWidth: 123, frameHeight: 165 });
+    this.load.spritesheet('king', 'assets/king.png', { frameWidth: 240, frameHeight: 420 });
 }
 
 function create() {
@@ -58,7 +59,7 @@ function create() {
     this.physics.add.collider(platforms, iceCreams, removeIceCream, null, this);
 
 
-    player = this.physics.add.sprite(100, floorHeight-128, 'boy').setScale(scaleRatio);
+    player = this.physics.add.sprite(100, floorHeight-180, 'king').setScale(scaleRatio*0.8);
     this.physics.add.collider(player, platforms);
 
     player.setBounce(0.2);
@@ -66,20 +67,20 @@ function create() {
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('boy', { start: 3, end: 5 }),
+        frames: this.anims.generateFrameNumbers('king', { start: 1, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'turn',
-        frames: [{ key: 'boy', frame: 0 }],
+        frames: [{ key: 'king', frame: 0 }],
         frameRate: 20
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('boy', { start: 3, end: 5 }),
+        frames: this.anims.generateFrameNumbers('king', { start: 1, end: 3 }),
         frameRate: 10,
         repeat: -1,
     });
